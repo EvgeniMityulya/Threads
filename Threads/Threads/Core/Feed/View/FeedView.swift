a56de5696f850d694bba8f0aa821e5198bb43fe6
iseedeadpeople <evgenni2004@gmail.com>
2024-09-15 13:18:04 +0300

    ADD FeedView

--- Threads/Threads/Core/Feed/View/FeedView.swift
+++ Threads/Threads/Core/Feed/View/FeedView.swift
@@ -9,10 +9,36 @@ import SwiftUI
 
 struct FeedView: View {
     var body: some View {
-        Text("Hello, Feed!")
+        NavigationStack {
+            ScrollView() {
+                LazyVStack {
+                    ForEach(0 ... 10, id: \.self) { thread in
+                        ThreadCell()
+                    }
+                }
+            }
+            .refreshable {
+                print("DEBUG: Refresh threads")
+            }
+            .scrollIndicators(.never)
+            .navigationTitle("Threads")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+        .toolbar {
+            ToolbarItem(placement: .topBarTrailing) {
+                Button {
+                    
+                } label: {
+                    Image(systemName: "arrow.counterclockwise")
+                        .foregroundStyle(Color(.black))
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    FeedView()
+    NavigationStack {
+        FeedView()
+    }
 }
---
+++ Threads/Threads/Core/Feed/View/ThreadCell.swift
@@ -0,0 +1,82 @@
+//
+//  ThreadCell.swift
+//  Threads
+//
+//  Created by Евгений Митюля on 9/15/24.
+//
+
+import SwiftUI
+
+struct ThreadCell: View {
+    var body: some View {
+        VStack {
+            HStack(alignment: .top, spacing: 12) {
+                Image("person1")
+                    .resizable()
+                    .scaledToFill()
+                    .frame(width: 40, height: 40)
+                    .clipShape(Circle())
+                
+                VStack(alignment: .leading, spacing: 4) {
+                    HStack {
+                        Text("person username")
+                            .font(.footnote)
+                            .fontWeight(.semibold)
+                        
+                        Spacer()
+                        
+                        Text("10m")
+                            .font(.caption)
+                            .foregroundStyle(Color(.systemGray3))
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "ellipsis")
+                                .foregroundStyle(Color(.darkGray))
+                        }
+                    }
+                    
+                    Text("person subtitle")
+                        .font(.footnote)
+                        .multilineTextAlignment(.leading)
+                    
+                    HStack(spacing: 16) {
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "heart")
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "bubble.right")
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "arrow.rectanglepath")
+                        }
+                        
+                        Button {
+                            
+                        } label: {
+                            Image(systemName: "paperplane")
+                        }
+                    }
+                    .foregroundStyle(Color(.black))
+                    .padding(.vertical, 8)
+                }
+            }
+            
+            Divider()
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ThreadCell()
+}
